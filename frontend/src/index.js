1dd18839b3b779f1cf01c085f49b296b9e04d01b
dong5053 <54871687+dong5053@users.noreply.github.com>
2024-05-23 14:30:04 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,15 +11,6 @@ root.render(
   </React.StrictMode>
 );
 
-// XSS 취약점 예제
-const urlParams = new URLSearchParams(window.location.search);
-const userName = urlParams.get('name');
-if (userName) {
-  const xssElement = document.createElement('div');
-  xssElement.innerHTML = `환영합니다, ${userName}`; // 취약한 부분
-  document.body.appendChild(xssElement);
-}
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
